6e0dce9098a6da31e6ad74ddd2f48c564f35adc2
Zgasp <42748080+Zgasp@users.noreply.github.com>
2019-03-03 22:17:03 -0600

    Added javascript to focus form inputs

--- site.js
+++ site.js
@@ -7,4 +7,15 @@
 	document.addEventListener('DOMContentLoaded',function(){
 		console.log('The DOM is loaded')
 	})
+
+	var name_input = document.querySelector('#name');
+  name_input.addEventListener('focus', function(){
+    console.log('Full name is focused');
+  });
+
+  var email_input = document.querySelector('#email');
+  email_input.addEventListener('focus', function(){
+    console.log('Email is focused');
+  });
+
 }());
