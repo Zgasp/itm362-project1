3ab3b9c8738b6335eb2d3f0886ab35fe479530c2
Zgasp <42748080+Zgasp@users.noreply.github.com>
2019-03-03 22:04:32 -0600

    Adjusted site.js indentation and cascade

--- site.js
+++ site.js
@@ -1,9 +1,10 @@
-if (!('querySelector' in document && 'addEventListener' in document)) {
-  //for old browsers
-  return;
-}//end if
-else{
-document.addEventListener('DOMContentLoaded',function(){
-	console.log('OMG the dom is loaded')
-})
-}//End else
+(function(){
+	if (!('querySelector' in document && 'addEventListener' in document)) {
+  	//for old browsers
+  	return;
+	}//end if
+
+	document.addEventListener('DOMContentLoaded',function(){
+		console.log('The DOM is loaded')
+	})
+}());
